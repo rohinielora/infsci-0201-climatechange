50b7a7fe1f82449eac762a77f89c54bf109e2fb9
rohinielora <70495118+rohinielora@users.noreply.github.com>
2024-03-23 20:37:01 -0400

    Update main.py

--- progress/main.py
+++ progress/main.py
@@ -1,1 +1,19 @@
+from DataAccess import DataAccess
+from Model import Model
+from View import View
+
+def main():
+    data_access = DataAccess()
+    model = Model(data_access)
+    view = View()
+
+    # Get processed temperature data and geojson from the model
+    temperature_data = model.get_temperature_data()
+    countries_geojson = model.get_geojson()
+
+    # Use the view to plot the temperature standard deviation
+    view.plot_temperature_std_dev(countries_geojson, temperature_data)
+
+if __name__ == "__main__":
+    main()
 
